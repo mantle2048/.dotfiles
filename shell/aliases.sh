e21ef563c2882b64fe8045fc3a1a8714fad21ba4
my871432122 <871432122@qq.com>
2021-02-09 17:06:22 +0800

    update

---
+++ shell/aliases.sh
@@ -0,0 +1,23 @@
+#Use colors in coreutils utilities output
+alias ls='ls --color=auto'
+alias grep='grep --color'
+
+# ls aliases
+alias ll='ls -lah'
+alias la='ls -A'
+alias l='ls'
+
+# Aliases to protect against overwriting
+alias cp='cp -i'
+alias mv='mv -i'
+alias rm='rm -i'
+
+# Create a directory and cd into it
+mcd() {
+    mkdir "${1}" && cd "${1}"
+}
+
+# other alias
+alias fd=fdfind
+alias cht=cht.s
+alias gs='git status'
