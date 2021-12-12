cc801201f92bc3023a9be516e42116867dd45e3f
my871432122 <871432122@qq.com>
2021-12-12 10:25:56 +0800

    Add shell useful functions (here there qextract qcompress etc.)

---
+++ shell/functions.sh
@@ -0,0 +1,85 @@
+path_remove() {
+    PATH=$(echo -n "$PATH" | awk -v RS=: -v ORS=: "\$0 != \"$1\"" | sed 's/:$//')
+}
+
+path_append() {
+    path_remove "$1"
+    PATH="${PATH:+"$PATH:"}$1"
+}
+
+path_prepend() {
+    path_remove "$1"
+    PATH="$1${PATH:+":$PATH"}"
+}
+
+here() {
+    local loc
+    if [ "$#" -eq 1 ]; then
+        loc=$(realpath "$1")
+    else
+        loc=$(realpath ".")
+    fi
+    ln -sfn "${loc}" "$HOME/.shell.here"
+    echo "here -> $(readlink $HOME/.shell.here)"
+}
+
+there="$HOME/.shell.here"
+
+there() {
+    cd "$(readlink "${there}")"
+}
+
+# copy from https://github.com/skywind3000/awesome-cheatsheets/blob/master/languages/bash.sh
+
+# 自动解压：判断文件后缀名并调用相应解压命令
+qextract() {
+    if [ -f $1 ] ; then
+        case $1 in
+        *.tar.bz2)   tar -xvjf $1    ;;
+        *.tar.gz)    tar -xvzf $1    ;;
+        *.tar.xz)    tar -xvJf $1    ;;
+        *.bz2)       bunzip2 $1     ;;
+        *.rar)       rar x $1       ;;
+        *.gz)        gunzip $1      ;;
+        *.tar)       tar -xvf $1     ;;
+        *.tbz2)      tar -xvjf $1    ;;
+        *.tgz)       tar -xvzf $1    ;;
+        *.zip)       unzip $1       ;;
+        *.Z)         uncompress $1  ;;
+        *.7z)        7z x $1        ;;
+        *)           echo "don't know how to extract '$1'..." ;;
+        esac
+    else
+        echo "'$1' is not a valid file!"
+    fi
+}
+
+# 自动压缩：判断后缀名并调用相应压缩程序
+qcompress() {
+    if [ -n "$1" ] ; then
+        FILE=$1
+        case $FILE in
+        *.tar) shift && tar -cf $FILE $* ;;
+        *.tar.bz2) shift && tar -cjf $FILE $* ;;
+        *.tar.xz) shift && tar -cJf $FILE $* ;;
+        *.tar.gz) shift && tar -czf $FILE $* ;;
+        *.tgz) shift && tar -czf $FILE $* ;;
+        *.zip) shift && zip $FILE $* ;;
+        *.rar) shift && rar $FILE $* ;;
+        esac
+    else
+        echo "usage: q-compress <foo.tar.gz> ./foo ./bar"
+    fi
+}
+
+# 漂亮的带语法高亮的 color cat ，需要先 pip install pygments
+ccat() {
+    local style="monokai"
+    if [ $# -eq 0 ]; then
+        pygmentize -P style=$style -P tabsize=4 -f terminal256 -g
+    else
+        for NAME in $@; do
+            pygmentize -P style=$style -P tabsize=4 -f terminal256 -g "$NAME"
+        done
+    fi
+}
