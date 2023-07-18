63220dd0aa67e1ea9eacc520813e0c60af96eaa5
my871432122 <871432122@qq.com>
2023-07-18 10:49:39 +0800

    disable auto formating and add cfg dot aliases

--- shell/aliases.sh
+++ shell/aliases.sh
@@ -22,48 +22,50 @@ alias sd='conda deactivate'
 alias cht=cht.sh
 alias gs='git status'
 alias nnn='nnn -de'
-alias data='cd /data3/nisyan/'
-alias home='cd ~'
+alias hg='cd ~'
 
 # alias vim2nvim
 alias vim='nvim'
 
+# easier to access dotfiles
+alias cfg='cd ~/.config/nvim/lua/user'
+alias dot='cd ~/.dotfiles'
+
 # Create a directory and cd into it
 mcd() {
-    mkdir "${1}" && cd "${1}"
+	mkdir "${1}" && cd "${1}"
 }
 
 # Jump to directory containing file
 jump() {
-    cd "$(dirname ${1})"
+	cd "$(dirname ${1})"
 }
 
 # Go up [n] directories
-up()
-{
-    local cdir="$(pwd)"
-    if [[ "${1}" == "" ]]; then
-        cdir="$(dirname "${cdir}")"
-    elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
-        echo "Error: argument must be a number"
-    elif ! [[ "${1}" -gt "0" ]]; then
-        echo "Error: argument must be positive"
-    else
-        for ((i=0; i<${1}; i++)); do
-            local ncdir="$(dirname "${cdir}")"
-            if [[ "${cdir}" == "${ncdir}" ]]; then
-                break
-            else
-                cdir="${ncdir}"
-            fi
-        done
-    fi
-    cd "${cdir}"
+up() {
+	local cdir="$(pwd)"
+	if [[ "${1}" == "" ]]; then
+		cdir="$(dirname "${cdir}")"
+	elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
+		echo "Error: argument must be a number"
+	elif ! [[ "${1}" -gt "0" ]]; then
+		echo "Error: argument must be positive"
+	else
+		for ((i = 0; i < ${1}; i++)); do
+			local ncdir="$(dirname "${cdir}")"
+			if [[ "${cdir}" == "${ncdir}" ]]; then
+				break
+			else
+				cdir="${ncdir}"
+			fi
+		done
+	fi
+	cd "${cdir}"
 }
 
 # Execute a command in a specific directory
 xin() {
-    (
-        cd "${1}" && shift && "${@}"
-    )
+	(
+		cd "${1}" && shift && "${@}"
+	)
 }
